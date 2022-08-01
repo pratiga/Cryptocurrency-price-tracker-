3fc5caf17a3d1bda13e668a23fe99528f1dc7823
pratiga <gaitrisanu2468@gmail.com>
2022-08-01 20:49:30 +0545

    initial commit

---
+++ src/app/coin-list/coin-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit,AfterViewInit, ViewChild } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+
+@Component({
+  selector: 'app-coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrls: ['./coin-list.component.scss']
+})
+export class CoinListComponent implements OnInit {
+  
+  bannerData : any=[];
+  dataSource!: MatTableDataSource<any> ;
+  displayedColumns: string[] = ['symbol','current_price', 'price_change_percentage_24h','market_cap'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator 
+  @ViewChild(MatSort) sort!: MatSort 
+  constructor(private api : ApiService) { }
+
+  ngOnInit(): void {
+    this.getAllData();
+    this.getBannerData();
+    
+  }
+  getBannerData(){
+    this.api.getTrendingCurrency('INR')
+    .subscribe((res: any)=>{
+      this.bannerData = res;
+    })
+  }
+  getAllData(){
+    this.api.getCurrency("INR")
+    .subscribe((res: any)=>{
+      console.log(res);
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+}
