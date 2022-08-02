1aa12d6cbf22d6a36e2c8d522a511f9c6d1c553b
pratiga <gaitrisanu2468@gmail.com>
2022-08-02 20:17:19 +0545

    coin details component
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../service/api.service';
+import { ChartConfiguration, ChartType} from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-coin-detail',
@@ -7,9 +11,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoinDetailComponent implements OnInit {
 
-  constructor() { }
+  coinData: any;
+  coinId !: string;
+  days : number = 1;
+  currency : string = "INR";
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets:[
+      {
+        data:[],
+        label: `Price Trends`,
+        backgroundColor: `rgba(148,159,177,0.2)`,
+        borderColor: `#009688`,
+        pointBackgroundColor: `#009688`,
+        pointBorderColor:`#009688`,
+      }
+    ],
+    labels: []
+  };
+  public lineChartOptions: ChartConfiguration['options'] = {
+    elements:{
+      point: {
+        radius: 1
+      }
+    },
+    
+    plugins:{
+      legend: { display:true},
+    }
+  };
+  public lineChartType: ChartType = 'line';
+  @ViewChild(BaseChartDirective)myLineChart !: BaseChartDirective;
+
+  constructor(private api : ApiService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(val=>{
+        this.coinId = val['id'];
+    });
+    this.getCoinData();
+    this.getGraphData(this.days);
+    
   }
+  getCoinData(){
+    this.api.getCurrencyById(this.coinId)
+    .subscribe(res=>{
+      this.coinData = res;
+      console.log(this.coinData)
+    })
+  }
+  
+  // getGraphData(days:number){
+  //   this.days = days
+  //   this.api.getGrpahicalCurrencyData(this.coinId,this.currency,this.days)
+  //   .subscribe(res=>{
+  // //     console.log(res);
+  // //   })
+  // // }
+  // // getGraphData(){
+  // //   this.api.getGrpahicalCurrencyData(this.coinId,"INR",1)
+  // //   .subscribe(res=> {
+  //    console.log(res);
+  //    this.lineChartData.datasets[0].data = res.prices.map((a:any)=>{
+  //      return a[1];
+  //    });
+  //    this.lineChartData.labels = res.prices.map((a:any)=>{
+  //      let date = new Date(a[0]);
+  //      let time = date.getHours() > 12 ?
+  //       `${date.getHours() -12}: ${date.getMinutes()} PM` :
+  //      `${date.getHours()}: ${date.getMinutes()} AM`
+  //       return this.days === 1 ? time : date.toLocaleDateString();
+  //     })
+  //   })
+  //  }
 
+
+  getGraphData(days:number){
+    this.days = days
+    this.api.getGrpahicalCurrencyData(this.coinId,this.currency,this.days)
+    .subscribe(res=>{
+      setTimeout(() => {
+        this.myLineChart.chart?.update();
+      }, 200);
+      this.lineChartData.datasets[0].data = res.prices.map((a:any)=>{
+        return a[1];
+      });
+      this.lineChartData.labels = res.prices.map((a:any)=>{
+        let date = new Date(a[0]);
+        let time = date.getHours() > 12 ?
+        `${date.getHours() - 12}: ${date.getMinutes()} PM` :
+        `${date.getHours()}: ${date.getMinutes()} AM`
+        return this.days === 1 ? time : date.toLocaleDateString();
+      })
+    })
+  }
 }
