71f08d7fae0b5d9dd1211c05d39ca3c87d730fa6
pratiga <gaitrisanu2468@gmail.com>
2022-08-02 20:15:58 +0545

    add routing
    Signed-off-by: pratiga <gaitrisanu2468@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { CoinListComponent } from './coin-list/coin-list.component';
 const routes: Routes = [
   {path:'', redirectTo:"coin-list", pathMatch:'full'},
   {path:"coin-list", component:CoinListComponent},
-  {path:"coin-details", component:CoinDetailComponent}
+  {path:"coin-detail/:id", component:CoinDetailComponent},
 ];
 
 @NgModule({
